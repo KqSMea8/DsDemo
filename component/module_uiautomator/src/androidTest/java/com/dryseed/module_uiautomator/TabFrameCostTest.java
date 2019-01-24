c8f9cf7735761783c0de2f13976ed8f14eab070a
caiminming <caiminming@qiyi.com>
2019-01-24 15:02:39 +0800

    uiautomator test gfxinfo

--- component/module_uiautomator/src/androidTest/java/com/dryseed/module_uiautomator/TabFrameCostTest.java
+++ component/module_uiautomator/src/androidTest/java/com/dryseed/module_uiautomator/TabFrameCostTest.java
@@ -15,13 +15,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author caiminming
  */
 @RunWith(AndroidJUnit4.class)
 public class TabFrameCostTest {
+
+    private static final String[] sStartLineTag = {"Draw", "Prepare", "Process", "Execute"};
+    private static final String[] sNameArray = {"Total", "Draw", "Prepare", "Process", "Execute"};
+
     private Instrumentation mInstrumentation;
     private Context mContext;
     private UiDevice mUiDevice;
@@ -52,7 +60,6 @@ public class TabFrameCostTest {
                 return;
             }
             view.click();
-            Thread.sleep(2000);
 
             //dumpsys gfxinfo
             Shell rootShell = new Shell.Builder().setRoot(true).build();
@@ -62,7 +69,90 @@ public class TabFrameCostTest {
                             "dumpsys gfxinfo \"" + MyAndroidJUnitRunner.getTargetProcessName() + "\""
                     )
             );
-            Log.e("MMM", result);
+
+            processData(result);
+
+            Thread.sleep(2000);
         }
+    }
+
+    private void processData(String result) {
+        Map<String, Float> dataMap = new LinkedHashMap<>();
+
+        BufferedReader bufferedReader = null;
+        try {
+            //bufferedReader = new BufferedReader(new FileReader(file));
+            bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            new ByteArrayInputStream(
+                                    result.getBytes(Charset.forName("utf8"))
+                            ),
+                            Charset.forName("utf8")
+                    )
+            );
+            String line = null;
+            boolean startTag = false;
+            Float[] timeArray = {0f, 0f, 0f, 0f, 0f};
+            int totalLineNum = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                // 显示行号
+                line = line.trim();
+
+                if ("".equals(line)) {
+                    continue;
+                } else if (line.startsWith("Draw")) {
+                    String[] subStrings = line.split("\t");
+                    if (Arrays.equals(subStrings, sStartLineTag)) {
+                        startTag = true;
+                    }
+                } else if (startTag) {
+                    String[] scores = line.split("\t");
+                    if (scores.length < 4) {
+                        continue;
+                    }
+                    Float temp1 = Float.parseFloat(scores[0]);
+                    Float temp2 = Float.parseFloat(scores[1]);
+                    Float temp3 = Float.parseFloat(scores[2]);
+                    Float temp4 = Float.parseFloat(scores[3]);
+                    if (Float.isNaN(temp1) || Float.isNaN(temp2) || Float.isNaN(temp3) || Float.isNaN(temp4)) {
+                        continue;
+                    }
+                    //draw
+                    timeArray[1] += temp1;
+                    //Prepare
+                    timeArray[2] += temp2;
+                    //Process
+                    timeArray[3] += temp3;
+                    //Execute
+                    timeArray[4] += temp4;
+                    //total
+                    timeArray[0] += temp1 + temp2 + temp3 + temp4;
+
+                    totalLineNum++;
+                }
+            }
+
+            for (int index = 0; index < timeArray.length; index++) {
+                if (totalLineNum > 0) {
+                    dataMap.put(sNameArray[index], timeArray[index] / totalLineNum);
+                }
+            }
+
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        Log.e("MMM", "dataMap " + dataMap);
     }
 }
--- component/module_uiautomator/src/test/java/com/dryseed/module_uiautomator/ExampleUnitTest.java
+++ component/module_uiautomator/src/test/java/com/dryseed/module_uiautomator/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.dryseed.module_uiautomator;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,83 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private static final String[] sStartLineTag = {"Draw", "Prepare", "Process", "Execute"};
+    private static final String[] sNameArray = {"Total", "Draw", "Prepare", "Process", "Execute"};
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void processGfxinfoData() {
+        File file = new File("src/test/java/com/dryseed/module_uiautomator/1.txt");
+        Assert.assertTrue(file.exists());
+
+        Map<String, Float> dataMap = new LinkedHashMap<>();
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            boolean startTag = false;
+            Float[] timeArray = {0f, 0f, 0f, 0f, 0f};
+            int totalLineNum = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                // 显示行号
+                line = line.trim();
+
+                if ("".equals(line)) {
+                    continue;
+                } else if (line.startsWith("Draw")) {
+                    String[] subStrings = line.split("\t");
+                    if (Arrays.equals(subStrings, sStartLineTag)) {
+                        startTag = true;
+                    }
+                } else if (startTag) {
+                    String[] scores = line.split("\t");
+                    if (scores.length < 4) {
+                        continue;
+                    }
+                    Float temp1 = Float.parseFloat(scores[0]);
+                    Float temp2 = Float.parseFloat(scores[1]);
+                    Float temp3 = Float.parseFloat(scores[2]);
+                    Float temp4 = Float.parseFloat(scores[3]);
+                    if (Float.isNaN(temp1) || Float.isNaN(temp2) || Float.isNaN(temp3) || Float.isNaN(temp4)) {
+                        continue;
+                    }
+                    //draw
+                    timeArray[1] += temp1;
+                    //Prepare
+                    timeArray[2] += temp2;
+                    //Process
+                    timeArray[3] += temp3;
+                    //Execute
+                    timeArray[4] += temp4;
+                    //total
+                    timeArray[0] += temp1 + temp2 + temp3 + temp4;
+
+                    totalLineNum++;
+                }
+            }
+
+            for (int index = 0; index < timeArray.length; index++) {
+                if (totalLineNum > 0) {
+                    dataMap.put(sNameArray[index], timeArray[index] / totalLineNum);
+                }
+            }
+
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        System.out.println("dataMap " + dataMap);
     }
 }
